d402d14ae70037f355e2a9a735c833a240259803
BuildTools <unconfigured@null.spigotmc.org>
2023-07-26 22:54:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var x = 10;
+
+/*
+while(x = 10) {
+    console.log("XD")
+}
+while(x = 10) {
+    console.log("XD")
+}
+while(x = 10) {
+    console.log("XD")
+}
+while(x = 10) {
+    console.log("XD")
+}
+while(x = 10) {
+    console.log("XD")
+}
+*/
